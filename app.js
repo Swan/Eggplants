8c692586b5e97705b2aba175d7a988f2e586b0e4
Swan <swan@reborn.com>
2017-02-08 15:26:49 -0500

    Remove v1 from the repository

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
-// Dependencies
-var express               = require("express"),
-    app                   = express(),
-    methodOverride        = require("method-override"), 
-    bodyParser            = require("body-parser"), 
-    expressSanitizer      = require("express-sanitizer"), 
-    passport              = require("passport"), 
-    LocalStrategy         = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose"), 
-    mongoose              = require("mongoose"), 
-    flash                 = require("connect-flash"),
-    indexRoutes           = require("./routes/index");
+// Express
+var express = require('express');
+var app = express();
 
-// Use Dependencies
-app.use(require("express-session")({
-    secret: "meme", 
-    resave: false,
-    saveUninitialized: false
-}));   
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(expressSanitizer());
-app.use(flash()); 
-app.use(function(req, res, next) {
-    res.locals.currentUser = req.user;
-    res.locals.errorMessage = req.flash("error");
-    res.locals.successMessage = req.flash("success");
+// Path
+var path = require('path');
+
+// Body-Parser
+var bodyParser = require('body-parser');
+
+// Require in our routes
+ var indexRoutes = require('./routes/index');
+
+// Cors
+ var cors = require('cors');
+
+// Setting Port
+app.set('port', 8080);
+
+// Logging GET & POST Requests
+app.use(function(req, res, next){
+    console.log(req.method, req.url);
     next();
-})
-app.use(passport.initialize()); 
-app.use(passport.session());
+});
 
-// Use routes inside routes folder
+
+// Use Public Folder
+app.use(express.static(__dirname + "/public"));
+
+// Use node_modules Folder
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+// Use Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Use cors
+app.use(cors({origin: 'http://localhost:8888'}));
+
+// Use Routes
 app.use(indexRoutes);
 
-// MongoDB
-//mongoose.connect("mongodb://localhost/eggplants");
-
-
-// Start Server
-app.listen(8080, function(){
-    console.log("Server Started");
+// Start server
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log("Server started on port: " + port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,26 @@
-// Dependencies
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var passport= require("passport");
-var bodyParser = require("body-parser");
-var request = require("request");
-var flash = require("connect-flash");
-var Eggplants = require("../middleware");
-var Listing = require("../middleware/listing.js");
 
-// Use Dependencies
-router.use(bodyParser.urlencoded({extended: true})); 
-router.use(flash()); 
-router.use(function(req, res, next) {
-    res.locals.currentUser = req.user;
-    res.locals.errorMessage = req.flash("error");
-    res.locals.successMessage = req.flash("success");
-    next();
-})
+var eggplants = require('../controllers/eggplants.js');
 
+// Downloading via /s/ links
+router
+    .route('/s/:id')
+    .get(eggplants.setIdDownload);
+
+// Download via /b/ links
+router
+    .route('/b/:id')
+    .get(eggplants.beatmapIdDownload);
 
 // Root
-router.get("/", function(req, res){
-    res.render("index");
-});
+router
+    .route('/')
+    .post(eggplants.direct);
+    
+          
 
-// Direct
-router.post("/", Eggplants.Direct, function(req, res){
-});
 
-// Beatmap Set Download Link
-router.get("/s/:id", Eggplants.SetIdDownload, function(req, res){
-});
-
-// Beatmap Id Download Link
-router.get("/b/:id", Eggplants.BeatmapIdDownload, function(req, res){
-});
-
-// Beatmap Listing
-router.get("/osu", Listing.StarterMaps, function(req, res){
-    res.render("osu");
-});
-
-// Beatmap Listing POST
-router.post("/osu", Listing.Beatmaps, function(req, res){
-});
-
-// 404
-router.get("*", function(req, res){
-   res.redirect("back"); 
-});
-
-// Export
 module.exports = router;
+
+    
