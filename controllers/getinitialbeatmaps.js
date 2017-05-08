7848e7459ffbe806e967128f684d23f2f0f4f9b0
Swan <swan@reborn.com>
2017-05-08 18:32:20 -0400

    Add: Get random mode's beatmaps upon page load

--- controllers/getinitialbeatmaps.js
+++ controllers/getinitialbeatmaps.js
@@ -4,8 +4,9 @@ const axios = require('axios');
 const getInitialBeatmaps = (req, res) => {
 
     let offset = req.query.offset; // TODO: Would need to increase the offset by 100 each time. 101, 201, 301 etc, for infinite scrolling
+    randomModeNum = Math.floor((Math.random() * 3) + 1);
 
-    axios.get(`http://storage.ripple.moe/api/search?amount=100&status=1&offset=${offset}`)
+    axios.get(`http://storage.ripple.moe/api/search?amount=100&status=1&offset=${offset}&mode=${randomModeNum}`)
         .then((response) => {
             res.json(response.data.Sets);
         })
