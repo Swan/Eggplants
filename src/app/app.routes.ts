2b1ed8a7afa680405585dcaac0f9cc9a55104a58
Swan <contact@swan.moe>
2017-05-17 18:10:55 -0400

    Add: Register & Login Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const APP_ROUTES: Routes = [
     // Home
     { path: '', component: HomeComponent, pathMatch: 'full' },
+    // Register
+    { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+    // login
+    { path: 'login', component: LoginComponent, pathMatch: 'full' },
 
     // 404
     { path: '**', redirectTo: '', pathMatch: 'full' }
