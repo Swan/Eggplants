4188d58e4eeb28fc4a18dc6d0e9489d0ef525da8
Swan <contact@swan.moe>
2017-05-18 14:29:26 -0400

    Add: Passport LocalStrategy for user auth

---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const User = require('../models/user.js');
+
+passport.use(new LocalStrategy({
+    usernameField: 'username'
+},
+    (username, password, done) => {
+        User.findOne({username}, (err, user) => {
+            // Return if err
+            if (err) return done(err);
+            // Return if user not found in database
+            if (!user) {
+                return done(null, false, {
+                    message: "User not found"
+                });
+            }
+            // Return if password is wrong
+            if (!user.validatePassword(password)) {
+                return done(null, false, {
+                    message: "Password was incorrect"
+                });
+            }
+
+            // If credentials were correct, return the user object
+            return done(null, user);
+        })
+    }
+))
