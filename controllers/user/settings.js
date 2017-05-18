644eb0eec13de7926e4401f5516576c69bf956c1
Swan <contact@swan.moe>
2017-05-18 15:53:10 -0400

    Add: Return user when successfully hitting the settings route

--- controllers/user/settings.js
+++ controllers/user/settings.js
@@ -1,3 +1,15 @@
+const mongoose = require('mongoose');
+const User = require('../../models/user');
+
 module.exports.userSettings = (req, res) => {
-    console.log("You are authenticated and hit the settings route!");
+    // If no user id exists in the JWT, return a 401
+    if (!req.payload._id) 
+        return res.status(401).json({status: 401, message: "UnauthorizedError: User not authenticated"});
+    else
+        // Return the user if they exist
+        User.findById(req.payload._id)
+            .exec((err, user) => {
+                if (err) return res.status(400).json({status: 500, message: "Bad Request"});
+                return res.status(200).json(user);
+            });  
 };
