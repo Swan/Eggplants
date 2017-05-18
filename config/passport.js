84e85b09646d67c944ffa07a16f9fbaf7b882901
Swan <contact@swan.moe>
2017-05-18 15:30:46 -0400

    Fix: Lowercase username before querying db

--- config/passport.js
+++ config/passport.js
@@ -4,27 +4,24 @@ const mongoose = require('mongoose');
 const User = require('../models/user.js');
 
 passport.use(new LocalStrategy({
-    usernameField: 'username'
-},
-    (username, password, done) => {
-        User.findOne({username}, (err, user) => {
-            // Return if err
-            if (err) return done(err);
-            // Return if user not found in database
-            if (!user) {
-                return done(null, false, {
-                    message: "User not found"
-                });
-            }
-            // Return if password is wrong
-            if (!user.validatePassword(password)) {
-                return done(null, false, {
-                    message: "Password was incorrect"
-                });
-            }
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true,
+        session: false
+    },
+    (req, username, password, done) => {
+    User.findOne({username: username.toLowerCase()}, (err, user) => {
+        // Return if err
+        if (err) return done(err);
 
-            // If credentials were correct, return the user object
-            return done(null, user);
-        })
-    }
+        // Return if user not found in database
+        if (!user) return done(null, false, {message: "User not found!"});
+        
+        // Return if password is wrong
+        if (!user.validatePassword(password)) return done(null, false, {message: "Password was incorrect"});
+    
+        // If credentials were correct, return the user object
+        return done(null, user);
+    })
+}
 ))
