92e5caf4aa6449bd7ca52d4408db66afb9524688
Swan <contact@swan.moe>
2017-05-20 16:38:19 -0400

    Add: Authentication Guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if (localStorage.getItem('currentUser')) {
+        // Logged in so return true
+        return true;        
+      }
+
+      this.router.navigate(['/login']);
+      return false;
+  }
+
+}
